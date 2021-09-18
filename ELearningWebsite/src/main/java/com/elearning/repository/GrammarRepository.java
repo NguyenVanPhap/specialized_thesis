62b143e38bb2654b6e1f86c7e50806fa0c5a8118
NguyenVanPhap <nguyenvanphap013@gmail.com>
2021-09-18 13:43:41 +0700

    Update Grammar

---
+++ ELearningWebsite/src/main/java/com/elearning/repository/GrammarRepository.java
@@ -0,0 +1,17 @@
+package com.elearning.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.elearning.entities.BaiGrammar;
+@Repository
+public interface GrammarRepository extends JpaRepository<BaiGrammar,Integer> {
+	List<BaiGrammar> findByBaigrammarid(int id);
+	
+	@Query("select grammar FROM BaiGrammar grammar WHERE grammar.tenbaigrammar LIKE CONCAT('%',:search,'%')")
+	List<BaiGrammar> searchGrammar( @Param("search") String search);
+}

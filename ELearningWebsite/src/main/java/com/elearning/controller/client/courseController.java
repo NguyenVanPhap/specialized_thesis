bd49b120c22e4c8a1178c0c69177d4ffd36dc038
NguyenVanPhap <nguyenvanphap013@gmail.com>
2021-10-02 15:34:28 +0700

    Add List cource , list grammar , grammar detail feature

---
+++ ELearningWebsite/src/main/java/com/elearning/controller/client/courseController.java
@@ -0,0 +1,17 @@
+package com.elearning.controller.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class courseController {
+
+	@GetMapping("/course")
+	public String course() {
+		return "client/list-course";
+
+	}
+}

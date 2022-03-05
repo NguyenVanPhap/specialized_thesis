795adbc56670b75a6118051dddac5d5a93bea863
NguyenVanPhap <nguyenvanphap013@gmail.com>
2022-03-05 09:59:01 +0700

    add reading exercise

---
+++ ELearningWebsite/src/main/java/com/elearning/controller/client/ReadingExerciseController.java
@@ -0,0 +1,32 @@
+package com.elearning.controller.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.elearning.entities.ReadingExercise;
+import com.elearning.service.ReadingExerciseService;
+
+@Controller
+@RequestMapping("/reading")
+public class ReadingExerciseController {
+
+	@Autowired
+	private ReadingExerciseService ReadingExerciseService;
+
+	@GetMapping("/part-{partNumber}/{id}")
+	public String baiDocPart(@PathVariable long id, @PathVariable int partNumber, Model model) {
+		ReadingExercise objReadingExercise = ReadingExerciseService.findReadingExerciseById(id).get();
+		model.addAttribute("ReadingExercise", objReadingExercise);
+		return "client/ReadingExercise/ReadingExercisePart" + partNumber;
+	}
+
+	@GetMapping("")
+	public String getAllBaiDocBy(Model model) {
+		return "client/ReadingExercise/listreadingexercise";
+	}
+
+}
---
+++ ELearningWebsite/src/main/java/com/elearning/repository/ReadingQuestionRepository.java
@@ -0,0 +1,16 @@
+package com.elearning.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.elearning.entities.ReadingQuestion;
+
+public interface ReadingQuestionRepository extends JpaRepository<ReadingQuestion, Long> {
+
+	Page<ReadingQuestion> findById(long id, Pageable pageable);
+
+	List<ReadingQuestion> findById(long id);
+}
